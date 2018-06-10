299ec872fa5eea38a66944fa38e5b7ec22275f46
Henrik Forstén <henrik.forsten@gmail.com>
2018-06-10 14:20:44 +0200

    Update fpu_estimate.py script

--- scripts/fpu_estimate.py
+++ scripts/fpu_estimate.py
@@ -21,12 +21,13 @@ def parse_chunk(chunk):
             root_eval.append(line[-1])
         elif e % 3 == 0:
             # Subtract root eval
-            winrates.append([i - root_eval[-1] for i in line])
+            winrates.append([i - root_eval[-1] if i != -1 else 0 for i in line])
         else:
-            scores.append([np.log(max(line)/(i + 1e-5)) for i in line])
+            scores.append(line)
     out = []
     for i in range(len(winrates)):
-        out.append(zip(winrates[i], scores[i]))
+        r = [root_eval[i]]*len(winrates[i])
+        out.append(zip(winrates[i], scores[i], r))
     return out
 
 chunks = glob.glob(sys.argv[1] + "*.gz")
@@ -43,37 +44,45 @@ for chunk in chunks:
 
 fpus = []
 scores = []
+root_evals = []
 for i in positions:
     for j in i:
         fpus.append(j[0])
         scores.append(j[1])
+        root_evals.append(j[2])
 
 fpus = np.array(fpus)
 scores = np.array(scores)
+root_evals = np.array(root_evals)
 
 reductions = np.linspace(-0.5, 0.5, 20)
 l2_loss = []
 for reduction in reductions:
-    l2_loss.append(np.sum( (fpus - reduction)**2 ))
+    l2_loss.append(1.0/(len(fpus))*np.sum( (fpus - reduction)**2 ))
 
-def func(x, a, b, c):
-    return a * np.exp(-b * x) + c
+def func(x, a):
+    return a*x[1]
 
-if 0:
-    coeff = np.polyfit(scores, fpus, 1)
-    fpu_est = np.poly1d(coeff)
-else:
-    popt, pcov = curve_fit(func, scores, fpus)
-    print popt
-    fpu_est = lambda x: func(x, *popt)
+#def func(x, a):
+#    return a
 
-l2 = np.sum( (fpus - fpu_est(scores))**2 )
-print l2, min(l2_loss)
+popt, pcov = curve_fit(func, [scores, root_evals], fpus)
+print(popt)
+fpu_est = lambda x: func(x, *popt)
+
+l2 = 0
+for i in range(len(fpus)):
+    l2 += (fpus[i] - fpu_est((scores[i], root_evals[i])))**2
+l2 /= len(fpus)
+print(l2, min(l2_loss))
 
 print('Best constant reduction: {}'.format(reductions[np.argmin(l2_loss)]))
 
+print(fpu_est((2e-2,0.5)))
+
 plt.figure()
 plt.hist(fpus, bins=50)
+plt.xlabel('FPU')
 
 plt.figure()
 plt.title('L2 loss of constant FPU reduction')
@@ -82,8 +91,37 @@ plt.xlabel('FPU reduction constant')
 plt.ylabel('L2 loss')
 
 plt.figure()
-plt.scatter(scores, fpus)
-x = np.linspace(min(scores), max(scores), 100)
-y = fpu_est(x)
-plt.plot(x, y, 'g')
+scores_sorted, fpus_sorted = zip(*sorted(zip(scores, fpus)))
+
+
+plt.scatter(scores_sorted, fpus_sorted)
+x1 = np.linspace(min(scores), max(scores), 500)
+x2_1 = np.array([0.1]*len(x1))
+x2_2 = np.array([0.5]*len(x1))
+x2_3 = np.array([0.9]*len(x1))
+y1 = [fpu_est(i) for i in zip(x1, x2_1)]
+y2 = [fpu_est(i) for i in zip(x1, x2_2)]
+y3 = [fpu_est(i) for i in zip(x1, x2_3)]
+plt.plot(x1, y1, 'g')
+plt.plot(x1, y2, 'g')
+plt.plot(x1, y3, 'g')
+plt.xlabel('Policy')
+plt.ylabel('FPU')
+
+plt.figure()
+root_evals_sorted, fpus_sorted = zip(*sorted(zip(root_evals, fpus)))
+x1 = np.linspace(min(root_evals), max(root_evals), 200)
+x2_1 = np.array([0.01]*len(x1))
+x2_2 = np.array([0.1]*len(x1))
+x2_3 = np.array([0.9]*len(x1))
+y1 = [fpu_est(i) for i in zip(x2_1, x1)]
+y2 = [fpu_est(i) for i in zip(x2_2, x1)]
+y3 = [fpu_est(i) for i in zip(x2_3, x1)]
+plt.scatter(root_evals_sorted, fpus_sorted)
+plt.plot(x1, y1, 'g')
+plt.plot(x1, y2, 'g')
+plt.plot(x1, y3, 'g')
+plt.xlabel('Root eval')
+plt.ylabel('FPU')
+
 plt.show()
