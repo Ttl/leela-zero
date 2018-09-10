e2f4e7890acb6b2d6abcda7ee04ffdf03ff9f425
Ancalagon <37827291+AncalagonX@users.noreply.github.com>
2018-09-11 05:37:41 +0900

    Speed up current "add_features" implementation
    As currently implemented, one leelaz.exe process is spawned to convert only one chunk before exiting. By adding the --cpu-only flag along with using a smaller 32x4 weights file, start-up time for each individual leelaz.exe process is vastly reduced. This saves an enormous amount of time when running "add_features" on many chunks.

--- training/tf/add_features.py
+++ training/tf/add_features.py
@@ -4,7 +4,7 @@ import os
 import glob
 import multiprocessing
 
-leela_cmd = "../../src/leelaz -w ../../64_10_se.txt"
+leela_cmd = "../../src/leelaz -w ../../32_4_se.txt --cpu-only"
 
 out_dir = "../../converted_chunks/"
 
